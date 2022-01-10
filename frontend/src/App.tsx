3e4bb5bfa74262f678756453f0e0b4a64d40e0b6
rbarbosa1985 <betoanselmo@gmail.com>
2022-01-10 15:32:53 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1>Oi</h1>
+    <Navbar />
+
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from '../../assets/github.svg';
+import './styles.css';
+
+function Navbar() {
+     return (
+          <header >
+               <nav className="container">
+                    <div className="dsmovie-nav-content">
+                         <h1>DSMovie</h1>
+                         <a href="https://github.com/rbarbosa1985" target="_blank" rel="noreferrer">
+                              <div className="dsmovie-contact-container">
+                                   <GithubIcon />
+                                   <p className="dsmovie-contact-link">/rbarbosa1985</p>
+                              </div>
+                         </a>
+                    </div>
+               </nav>
+          </header>
+     );
+}
+
+export default Navbar;
